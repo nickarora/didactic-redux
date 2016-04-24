92a210d03f1972c9b304ab634b09c21dcffa7a72
Nick Arora <arora.nick@gmail.com>
2016-04-24 00:44:03 -0700

    add header component to app

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
-const App = () => <p>Welcome to the App!</p>
+import { Grid } from 'react-bootstrap'
+
+import Header from './header.jsx'
+
+const App = () => <div>
+  <Header />
+  <Grid><p>Welcome to the Zapp!</p></Grid>
+</div>
 
 export default App
