187a259307e39999fa38661dff605f7fa0505a8c
Nick Arora <arora.nick@gmail.com>
2016-04-24 01:20:53 -0700

    create store using reducer

--- app/index.jsx
+++ app/index.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { browserHistory, IndexRoute, Route, Router } from 'react-router'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import './main.css'
 import App from './components/app.jsx'
 import Home from './components/home.jsx'
 import About from './components/about.jsx'
 
+import reducer from './reducer.js'
+
+const store = createStore(reducer)
+
 const div = document.createElement('div')
 
 document.body.appendChild(div)
 
-render(<Router history={browserHistory}>
-  <Route path='/' component={App}>
-    <IndexRoute component={Home} />
-    <Route path='about' component={About} />
-  </Route>
-</Router>, div)
+render(<Provider store={store}>
+  <Router history={browserHistory}>
+    <Route path='/' component={App}>
+      <IndexRoute component={Home} />
+      <Route path='about' component={About} />
+    </Route>
+  </Router>
+</Provider>, div)
